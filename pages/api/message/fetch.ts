747df6533cfe5bbdfbf73fe23f57538504ab2cca
lyc8503 <admin@lyc8503.site>
2023-07-28 15:33:11 +0800

    plain message

---
+++ pages/api/message/fetch.ts
@@ -0,0 +1,43 @@
+import type { NextApiRequest, NextApiResponse } from 'next'
+import { jwtVerify } from '../cryptography/jwt'
+import { JWT_SECRET } from '../config'
+import { createRedisInstance } from '../database/redis'
+
+type Payload = {
+  token?: string
+}
+
+
+export default async function handler(req: NextApiRequest, res: NextApiResponse) {
+
+  console.log("FETCH")
+
+  if (req.method !== 'GET') {
+    res.status(405).json({ msg: 'Method Not Allowed' })
+    return
+  }
+
+  const payload: Payload = req.query
+  const redis = createRedisInstance()
+
+  let identity: { nickname?: string, sub?: string };
+  try {
+    identity = jwtVerify(payload.token || "", JWT_SECRET)
+  } catch(e) {
+    console.log(e);
+    res.status(401).json({ msg: 'Unauthorized: ' + e })
+    return
+  }
+
+  res.setHeader('Content-Type', 'text/event-stream;charset=utf-8');
+  res.setHeader('Cache-Control', 'no-cache, no-transform');
+
+  redis.on('message', (channel, message) => {
+    if (channel !== 'msg_channel') return
+    res.write(message + "\n")
+  })
+  
+  await redis.subscribe('msg_channel')
+  // Never resolve
+  await new Promise(() => 0)
+}
---
+++ pages/api/message/plain.ts
@@ -0,0 +1,53 @@
+import type { NextApiRequest, NextApiResponse } from 'next'
+import { type } from 'os'
+import { jwtVerify } from '../cryptography/jwt'
+import { JWT_SECRET } from '../config'
+import { createRedisInstance } from '../database/redis'
+
+type Data = {
+  msg: string
+}
+
+type Payload = {
+  token: string,
+  message: string
+}
+
+
+export default async function handler(req: NextApiRequest, res: NextApiResponse<Data>) {
+
+  if (req.method !== 'POST') {
+    res.status(405).json({ msg: 'Method Not Allowed' })
+    return
+  }
+
+  const payload: Payload = req.body
+  const redis = createRedisInstance()
+
+  let identity: { nickname?: string, sub?: string };
+  try {
+    identity = jwtVerify(payload.token, JWT_SECRET)
+  } catch(e) {
+    console.log(e);
+    res.status(401).json({ msg: 'Unauthorized: ' + e })
+    return
+  }
+
+  const message = payload.message
+  const nickname = identity.nickname
+  const sub = identity.sub
+
+  console.log(`Message from ${nickname}(${sub}): ${message}`)
+
+  const msg = {
+    type: 'plain',
+    message: message,
+    nickname: nickname,
+    subject: sub
+  }
+
+  await redis.lpush('messages', JSON.stringify(msg))
+  await redis.publish('msg_channel', JSON.stringify(msg))
+
+  res.status(200).json({ msg: 'success' })
+}
